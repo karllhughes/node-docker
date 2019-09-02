d7bce5ac991ba4cbc243b7afb81b014438f10330
Karl Hughes <khughes.me@gmail.com>
2019-09-02 11:45:10 -0500

    Fixing test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from web --build
+docker-compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from test --build
